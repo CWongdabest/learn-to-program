e5a5030ca00aad8ec3e9e8d2ea89cf6de571b2e5
CWong <canacewongtoday@hotmail.com>
2017-09-10 22:52:39 +0000

    Chapter 5 exercises

---
+++ chapter5/betternumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favourite number?'
+number = gets.to_i
+betternumber = number + 1
+puts 'So your favourite number is ' + number.to_s + '. It is nice but I think ' + betternumber.to_s + ' is a better and bigger number!'
---
+++ chapter5/fullname.rb
@@ -0,0 +1,7 @@
+puts 'Hello! What is your first name?'
+firstname = gets.chomp
+puts 'What about your second name?'
+secondname = gets.chomp
+puts 'What about your last name?'
+lastname = gets.chomp
+puts 'Oh I see! So your full name is ' + firstname + ' ' + secondname + ' ' + lastname + '! That is a long name!' 
