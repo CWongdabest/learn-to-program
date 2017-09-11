90139a213fc18a9fa5a9b7fcfd5a9a47d0176440
CWong <canacewongtoday@hotmail.com>
2017-09-11 14:17:27 +0000

    Chapter 6 exercises

---
+++ chapter6/angryboss.rb
@@ -0,0 +1,3 @@
+puts 'So what do you want?'
+wants = gets.chomp.upcase
+puts 'WHADDAYA MEAN "' + wants + '"?!? YOU\'RE FIRED!'
---
+++ chapter6/table_of_contents.rb
@@ -0,0 +1,6 @@
+line_length = 60
+puts ('Table of Contents'.center(line_length))
+puts ('Chapter 1: Getting Started'.ljust(line_length/2) + 'page  1'.rjust(line_length/2))
+puts ('Chapter 2: Numbers'.ljust(line_length/2) + 'page  9'.rjust(line_length/2))
+puts ('Chapter 3: Letters'.ljust(line_length/2) + 'page 13'.rjust(line_length/2))
+
